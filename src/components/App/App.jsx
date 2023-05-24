965df2694dbe74feb76768eba42afba452b5ce06
andreev-oe <andreev.oe@yandex.ru>
2023-05-24 09:28:55 +0300

    feat: add mobile layout and styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,26 +12,28 @@ function App() {
       <div className={classes['content-wrapper']}>
         <section className={classes.transfers}>
           <h2 className={classes['transfers-title']}>Количество пересадок</h2>
-          <label htmlFor="1" className={classes.transfer}>
-            <input id="1" type="checkbox" className={classes.checkbox} />
-            <p className={classes['transfer-title']}>Все</p>
-          </label>
-          <label htmlFor="2" className={classes.transfer}>
-            <input id="2" type="checkbox" className={classes.checkbox} />
-            <p className={classes['transfer-title']}>Без пересадок</p>
-          </label>
-          <label htmlFor="3" className={classes.transfer}>
-            <input id="3" type="checkbox" className={classes.checkbox} />
-            <p className={classes['transfer-title']}>1 пересадка</p>
-          </label>
-          <label htmlFor="4" className={classes.transfer}>
-            <input id="4" type="checkbox" className={classes.checkbox} />
-            <p className={classes['transfer-title']}>2 пересадки</p>
-          </label>
-          <label htmlFor="5" className={classes.transfer}>
-            <input id="5" type="checkbox" className={classes.checkbox} />
-            <p className={classes['transfer-title']}>3 пересадки</p>
-          </label>
+          <div className={classes.variants}>
+            <label htmlFor="1" className={classes.transfer}>
+              <input id="1" type="checkbox" className={classes.checkbox} />
+              <p className={classes['transfer-title']}>Все</p>
+            </label>
+            <label htmlFor="2" className={classes.transfer}>
+              <input id="2" type="checkbox" className={classes.checkbox} />
+              <p className={classes['transfer-title']}>Без пересадок</p>
+            </label>
+            <label htmlFor="3" className={classes.transfer}>
+              <input id="3" type="checkbox" className={classes.checkbox} />
+              <p className={classes['transfer-title']}>1 пересадка</p>
+            </label>
+            <label htmlFor="4" className={classes.transfer}>
+              <input id="4" type="checkbox" className={classes.checkbox} />
+              <p className={classes['transfer-title']}>2 пересадки</p>
+            </label>
+            <label htmlFor="5" className={classes.transfer}>
+              <input id="5" type="checkbox" className={classes.checkbox} />
+              <p className={classes['transfer-title']}>3 пересадки</p>
+            </label>
+          </div>
         </section>
         <section className={classes['tickets-wrapper']}>
           <div className={classes.filters}>
